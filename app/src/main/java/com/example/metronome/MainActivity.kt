84c9e41b8e933cb8751374744e1b364fc6e05ad0
Patryk Baran <patryk.baran2.stud@pw.edu.pl>
2022-01-23 19:55:07 +0100

    added plus and minut buttons functionallity with editText null exceptions

--- app/src/main/java/com/example/metronome/MainActivity.kt
+++ app/src/main/java/com/example/metronome/MainActivity.kt
@@ -2,10 +2,35 @@ package com.example.metronome
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+        plusImageButton.setOnClickListener {increaseInt()}
+        minusImageButton.setOnClickListener {decreaseInt()}
+    }
+
+    private fun increaseInt() {
+        if (TextUtils.isEmpty(bpmAmountTextNumber.text.toString())){
+            bpmAmountTextNumber.setText("1")}
+        else
+        display(bpmAmountTextNumber.text.toString().toInt()+1)
+    }
+
+    private fun decreaseInt() {
+        if (TextUtils.isEmpty(bpmAmountTextNumber.text.toString())){
+            bpmAmountTextNumber.setText("0")
+        }else
+        display(bpmAmountTextNumber.text.toString().toInt()-1)
+
+    }
+
+    private fun display (number: Int){
+        bpmAmountTextNumber.setText("$number")
     }
 }
