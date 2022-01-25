1ceaaefc3baa22dd0493040000ce3acf13e64a69
Patryk Baran <patryk.baran2.stud@pw.edu.pl>
2022-01-26 00:17:14 +0100

    added start/stop button; added sound; limited input only to INTnumbers

--- app/src/main/java/com/example/metronome/MainActivity.kt
+++ app/src/main/java/com/example/metronome/MainActivity.kt
@@ -1,18 +1,26 @@
 package com.example.metronome
 
+import android.media.MediaPlayer
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.text.TextUtils
+import android.text.method.HideReturnsTransformationMethod
+import android.widget.Chronometer
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var mMediaPlayer: MediaPlayer? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
 
+        bpmAmountTextNumber.transformationMethod = HideReturnsTransformationMethod.getInstance()
         plusImageButton.setOnClickListener {increaseInt()}
         minusImageButton.setOnClickListener {decreaseInt()}
+        startStopButton.setOnClickListener {startBeat()}
     }
 
     private fun increaseInt() {
@@ -23,14 +31,55 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun decreaseInt() {
-        if (TextUtils.isEmpty(bpmAmountTextNumber.text.toString())){
-            bpmAmountTextNumber.setText("0")
-        }else
-        display(bpmAmountTextNumber.text.toString().toInt()-1)
+        when {
+            TextUtils.isEmpty(bpmAmountTextNumber.text.toString()) -> {
+                bpmAmountTextNumber.setText("0")
+            }
+            bpmAmountTextNumber.text.toString() == "0" -> {
+                bpmAmountTextNumber.setText("0")
+            }
+            else -> display(bpmAmountTextNumber.text.toString().toInt()-1)
+        }
 
     }
 
     private fun display (number: Int){
         bpmAmountTextNumber.setText("$number")
     }
+
+    private fun startBeat (){
+        var breakTime: Long = 60/bpmAmountTextNumber.text.toString().toLong()
+
+        if (startStopButton.text.toString() == "START"){
+            startStopButton.text = "STOP"
+            startStopButton.setBackgroundResource(R.drawable.stop_button)
+        }else{
+            startStopButton.text = "START"
+            startStopButton.setBackgroundResource(R.drawable.start_button)
+        }
+    }
+
+    fun playSound() {
+        if (mMediaPlayer == null) {
+            mMediaPlayer = MediaPlayer.create(this, R.raw.metronome_sound)
+            mMediaPlayer!!.isLooping = false
+            mMediaPlayer!!.start()
+        } else mMediaPlayer!!.start()
+    }
+
+    fun stopSound() {
+        if (mMediaPlayer != null) {
+            mMediaPlayer!!.stop()
+            mMediaPlayer!!.release()
+            mMediaPlayer = null
+        }
+    }
+    override fun onStop() {
+        super.onStop()
+        if (mMediaPlayer != null) {
+            mMediaPlayer!!.release()
+            mMediaPlayer = null
+        }
+    }
+
 }
